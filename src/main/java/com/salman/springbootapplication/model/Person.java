3803a943b065b587440db60431a80b9a5fb883d6
salman1819 <salmanshanu99@gmail.com>
2020-06-17 18:35:28 -0500

    push to see if a build triggers on the CI on the CI server.
    Signed-off-by: salman1819 <salmanshanu99@gmail.com>

--- src/main/java/com/salman/springbootapplication/model/Person.java
+++ src/main/java/com/salman/springbootapplication/model/Person.java
@@ -1,16 +1,18 @@
 package com.salman.springbootapplication.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import java.util.UUID;
 
-
 @Getter
 @Setter
 @RequiredArgsConstructor
 public class Person {
+    @JsonProperty("name")
     private final UUID id;
+    @JsonProperty("id")
     private final String name;
     
 }
